bd03369b996eb263d0e6263362486dccdfb9604d
samylledutra <samyalexsandra.dutra@gmail.com>
2024-08-01 19:54:58 -0300

    first commit

---
+++ src/main/java/br/edu/iftm/prova1/Prova1Application.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.prova1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova1Application.class, args);
+	}
+
+}
